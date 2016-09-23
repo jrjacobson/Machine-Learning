26bf88f8e8ffae6ee85ea7af6ef292e982bcb98b
jrjacobson <jrjacobson86@gmail.com>
2016-09-23 11:38:51 -0600

    added a car matrix

--- menu.py
+++ menu.py
@@ -4,12 +4,18 @@ import sys
 from sklearn import datasets
 import pandas as pd
 
+
+def adjustCar(data):
+    car_matrix = data.as_matrix()
+    return car
+
+
 # load data
 iris = datasets.load_iris()
-car = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data',
+car_pd = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data',
                   header=None,
                   usecols=[0, 1, 2, 3, 5, 6, 7])
-
+car = adjustCar(car_pd)
 cancer = datasets.load_breast_cancer()
 
 
